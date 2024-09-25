e61c5f6dffec5f49abe4ae12197a9d6160c719ff
hearsay316 <hearsay316@gmail.com>
2024-09-25 14:32:53 +0800

    feat: 添加错误处理机制和serde依赖引入了新的错误处理模块，实现了自定义错误类型`MyError`，并利用了`thiserror` crates来简化错误衍生过程。`MyError`目前涵盖了I/O错误、解析错误、JSON序列化错误和自定义错误消息。此外，项目现在依赖于`serde`和`serde_json`进行数据序列化和反序列化，以及`anyhow` crate提供更丰富的错误处理功能。
    此更新还包含了对错误处理和依赖项的初步整合，在`src/main.rs`和`src/lib.rs`文件中进行了调整，引入了必要的模块和错误处理逻辑。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::num::ParseIntError;
+use ecosystem::MyError;
+use anyhow::{Context, Error};
+
+fn main()->Result<(),Error> {
+    println!("size of anyhowError is {}",size_of::<Error>());
+    println!("size of io::Error is {}",size_of::<std::io::Error>());
+    println!("size of ParseIntError is {}",size_of::<ParseIntError>());
+    println!("size of serde_json::Error is {}",size_of::<serde_json::Error>());
+    println!("size of String is {}",size_of::<String>());
+    println!("size of MyError is {}",size_of::<MyError>());
+    let filename = "./Cargo.toml";
+    let fd = fs::File::open(filename).with_context(||format!("Can not find file :{}",filename))?;
+    fail_with_error()?;
+    Ok(())
+}
+fn fail_with_error() ->Result<(),MyError>{
+    Err(MyError::Custom("This is a custom error".to_string()))
 }
