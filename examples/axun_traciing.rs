0662548adfe5b17ec8c4eba470b0eeffa2cc00ae
hearsay316 <hearsay316@gmail.com>
2024-09-25 17:32:13 +0800

    markdownfeat(tracing): 实现滚动日志并配置时间格式
    实现每日滚动日志功能，将日志文件输出到`./tem/logs`目录下，主文件名为`ecosystem.log`。同时，配置日志时间格式为本地时区，并设置控制台日志级别为`DEBUG`，文件日志级别为`INFO`。

---
+++ examples/axun_traciing.rs
@@ -0,0 +1,71 @@
+use std::time::Duration;
+
+use axum::Router;
+use axum::routing::get;
+use time::macros::format_description;
+use tokio::net::TcpListener;
+use tokio::time::{sleep, Instant};
+use tracing::{info, level_filters::LevelFilter, instrument, warn, debug};
+
+use tracing_subscriber::{fmt, Layer};
+use tracing_subscriber::fmt::format::FmtSpan;
+
+use tracing_subscriber::fmt::time::OffsetTime;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    //秒
+    let local_time = OffsetTime::new(
+        time::UtcOffset::from_hms(8, 0, 0)?,
+        format_description!("[year]-[month]-[day] [hour]:[minute]:[second]"),
+    );
+    // tracing_subscriber::fmt::init();
+    let file_appender = tracing_appender::rolling::daily("./tem/logs", "ecosystem.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    let console = fmt::Layer::new()
+        .with_span_events(FmtSpan::CLOSE)
+        .pretty()
+        .with_timer(local_time.clone())
+        .with_filter(LevelFilter::DEBUG);
+
+    let file = fmt::Layer::new()
+        .with_span_events(FmtSpan::CLOSE)
+        .pretty()
+        .with_timer(local_time)
+        .with_writer(non_blocking)
+        .with_filter(LevelFilter::INFO);
+
+    tracing_subscriber::registry().with(console).with(file).init();
+    let addr = "0.0.0.0:9000";
+    let app = Router::new().route(
+        "/", get(index_handler),
+    );
+    info!("Starting  server on {}",addr);
+    let listener = TcpListener::bind(addr).await?;
+    axum::serve(listener, app.into_make_service()).await?;
+    /*
+    service
+    into_make_service
+    into_make_serive
+    */
+    Ok(())
+}
+#[instrument]
+async fn index_handler() -> &'static str {
+    debug!(" index handler started");
+    sleep(Duration::from_millis(10)).await;
+    let ret = long_task().await;
+    info!(http.status = 200,"index handler Request completed");
+    ret
+}
+#[instrument]
+async fn long_task() -> &'static str {
+    let start = Instant::now();
+    let dur = 112;
+    sleep(Duration::from_millis(dur)).await;
+    let elapsed = start.elapsed().as_millis() as u64;
+    warn!(app.task_duration=elapsed,"task long  too long");
+    "hello world"
+}
--- examples/err.rs
+++ examples/err.rs
@@ -1,8 +1,30 @@
 use std::fs;
 use std::num::ParseIntError;
-use ecosystem::MyError;
 use anyhow::{Context, Error};
+use thiserror::Error;
 
+#[derive(Error,Debug)]
+pub enum MyError{
+    #[error("I/O error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("Parse error:{0}")]
+    Parse(#[from] std::num::ParseIntError),
+    #[error("Serialize json error:{0}")]
+    Serialize(#[from] serde_json::Error),
+    #[error("Error:{0:?}")]
+    BigError(Box<BigError>),
+
+    #[error("Custom error :{0}")]
+    Custom(String)
+}
+#[allow(unused)]
+#[derive(Debug)]
+struct BigError{
+    a:String,
+    b:Vec<String>,
+    c:[u8;64],
+    d:i64
+}
 fn main()->Result<(),Error> {
     println!("size of anyhowError is {}",size_of::<Error>());
     println!("size of io::Error is {}",size_of::<std::io::Error>());
