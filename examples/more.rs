9072526a779b76a82ae075d2fa4e9c0e52eff393
hearsay316 <hearsay316@gmail.com>
2024-09-26 18:05:10 +0800

    feat:
    添加了`derive_more`特性，以支持扩展数据类型定义。
    
    利用`derive_more` crate，为类型和枚举添加了额外的派生实现，包括加法、显示、从其他类型转换和转换为其他类型的功能。这提供了更简洁、更强大的数据类型定义方法。

---
+++ examples/more.rs
@@ -0,0 +1,32 @@
+use std::fmt::Formatter;
+use derive_more::{Add, Display, From, Into};
+
+#[derive(PartialEq,Copy, Clone,From,Add,Into,Display)]
+struct MyInt(i32);
+
+
+#[derive(PartialEq)]
+struct Point2D{
+    x:i32,
+    y:i32
+}
+
+#[derive(PartialEq,From,Add,Display,Debug)]
+enum MyEnum{
+    #[display("int :{_0}")]
+    Int(i32),
+    Unit(u32),
+    #[display("nothing")]
+    Noting
+}
+fn main() -> anyhow::Result<()> {
+    let a  =  MyInt::from(10);
+    let b = MyInt::from(10);
+    let c:MyInt = 10.into();
+    println!("{}",a+c);
+    let e:MyEnum = 10i32.into();
+    let e1:MyEnum = 11u32.into();
+    let e3:MyEnum = MyEnum::Noting;
+    println!("e: {e:?}  e1: {e1:?}  e3 {e3:?}");
+    Ok(())
+}
