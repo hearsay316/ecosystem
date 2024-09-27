53c034a1a8ca8ae4fdcac12e40a3870eda7ff3c7
hearsay316 <hearsay316@gmail.com>
2024-09-27 16:08:37 +0800

    feat: ```添加bytes示例并更新Cargo依赖
    新增一个示例，展示如何使用`bytes` crate处理字节数据。示例包括如何创建、操作和分割字节数据。此外，`Cargo.lock`和`Cargo.toml`文件已更新，以包含对`bytes` crate的依赖。
    ```

---
+++ examples/bytes.rs
@@ -0,0 +1,20 @@
+use bytes::{BufMut, BytesMut};
+
+fn main() -> anyhow::Result<()> {
+
+    let mut buf = BytesMut::with_capacity(1024);
+    buf.extend_from_slice(b"hello word\\");
+
+    buf.put(&b"goodbye word"[..]);
+    buf.put_i32(12);
+    println!("{:?}",buf);
+    let a = buf.split();
+    let mut  b = a.freeze();
+    // let data_to_search = &b"10"[..];
+    // let pos = b.binary_search(data_to_search).unwrap();
+    let c = b.split_to(12);
+    println!("{:?}",b);
+    println!("{:?}",c);
+    println!("{:?}",buf);
+    Ok(())
+}
