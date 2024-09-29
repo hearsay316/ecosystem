1aebca05af1090806fce70f3befa52273b8e64ca
hearsay316 <hearsay316@gmail.com>
2024-09-29 21:27:42 +0800

    ```markdownfeat(url-shortener): 实现基于PostgreSQL的URL缩短服务
    使用Axum Web框架和PostgreSQL数据库实现了一个URL缩短服务。该服务允许缩短URL并通过HTTP 302重定向到原始URL。实现了基本的数据库迁移以创建必要的数据库表，并处理了URL的插入和检索逻辑。
    ```

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 链接的docker :
 
 docker run -d -p16686:16686 -p4317:4317 -e COLLECTOR_OTLP_ENABLED=true jaegertracing/all-in-one:latest
+
+检查
+cargo fmt -- --check
+
+cargo clippy --all-targets --all-features --tests --benches -- -D warnings
+
+修复
+cargo fmt 
---
+++ examples/shortener.rs
@@ -0,0 +1,123 @@
+use axum::extract::{Path, State};
+use axum::response::IntoResponse;
+use axum::routing::{get, post};
+use axum::{Json, Router};
+use http::header::LOCATION;
+use http::{HeaderMap, StatusCode};
+use nanoid::nanoid;
+use serde::{Deserialize, Serialize};
+use sqlx::{FromRow, PgPool};
+use tokio::net::TcpListener;
+use tracing::info;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::fmt::Layer;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::Layer as _;
+const LISTEN_ADDR: &str = "localhost:9876";
+#[derive(Debug, Deserialize)]
+struct ShortenReq {
+    url: String,
+}
+#[derive(Debug, Serialize)]
+struct ShortenRes {
+    url: String,
+}
+
+#[derive(Debug, Clone)]
+struct AppState {
+    db: PgPool,
+}
+
+#[derive(Debug, Clone, FromRow)]
+struct UrlRecord {
+    #[sqlx(default)]
+    id: String,
+    #[sqlx(default)]
+    url: String,
+}
+impl AppState {
+    async fn try_new(url: &str) -> anyhow::Result<Self> {
+        let pool = PgPool::connect(url).await?;
+        //创建 db
+        sqlx::query(
+            r#"
+            CREATE TABLE IF NOT EXISTS shortener (
+            id CHAR(6) PRIMARY KEY,
+            url TEXT NOT NULL UNIQUE
+            )
+    "#,
+        )
+        .execute(&pool)
+        .await
+        .expect("这个是个错误");
+        Ok(Self { db: pool })
+    }
+    async fn shorten(&self, url: &str) -> anyhow::Result<String> {
+        let id = nanoid!(6);
+        let ids:UrlRecord =   sqlx::query_as("INSERT INTO urls (id, url) VALUES ($1, $2) ON CONFLICT(url) DO UPDATE SET url=EXCLUDED.url RETURNING id")
+            .bind(&id)
+            .bind(url)
+            .fetch_one(&self.db)
+            .await?;
+        info!("{:?}", ids);
+        Ok(ids.id)
+    }
+    async fn get_url(&self, id: &str) -> anyhow::Result<String> {
+        let record: UrlRecord = sqlx::query_as("SELECT url FROM urls WHERE id = $1")
+            .bind(id)
+            .fetch_one(&self.db)
+            .await?;
+        println!("{:?}", record);
+        Ok(record.url)
+    }
+}
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    log_init();
+    let listener = TcpListener::bind(&LISTEN_ADDR).await?;
+    info!("Listener on :{}", LISTEN_ADDR);
+    let url = "postgres://postgres:123321@127.0.0.1:5432/shortener";
+    let state = AppState::try_new(url).await?;
+    info!(" Connected to database :{}", url);
+    let app = Router::new()
+        .route("/", post(shorten))
+        .route("/:id", get(redirect))
+        .with_state(state);
+    axum::serve(listener, app.into_make_service()).await?;
+    Ok(()) //into_make_service
+}
+// body 需要在最后以后一个参数
+async fn shorten(
+    State(state): State<AppState>,
+    Json(data): Json<ShortenReq>,
+) -> anyhow::Result<impl IntoResponse, StatusCode> {
+    let id = state
+        .shorten(&data.url)
+        .await
+        .map_err(|_| StatusCode::UNPROCESSABLE_ENTITY)?;
+    let body = Json(ShortenRes {
+        url: format!("http://{}/{}", LISTEN_ADDR, id),
+    });
+    Ok((StatusCode::CREATED, body))
+}
+async fn redirect(
+    Path(id): Path<String>,
+    State(state): State<AppState>,
+) -> anyhow::Result<impl IntoResponse, StatusCode> {
+    info!("这个是测试 {}", id);
+    let url = state
+        .get_url(&id)
+        .await
+        .map_err(|_| StatusCode::NOT_FOUND)?;
+
+    let mut header = HeaderMap::new();
+    header.insert(LOCATION, url.parse().unwrap());
+    info!("到了泽瑞");
+    Ok((StatusCode::FOUND, header))
+}
+fn log_init() {
+    // let console_layer = console_subscriber::spawn();
+    let layer = Layer::new().pretty().with_filter(LevelFilter::INFO);
+    tracing_subscriber::registry().with(layer).init();
+}
